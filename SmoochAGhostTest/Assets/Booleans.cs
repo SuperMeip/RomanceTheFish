90eee699534fab0f43cebee5ae3391aef0f411df
Meep <supermeip@gmail.com>
2016-01-28 17:53:24 -0500

    Added Test Doc

---
+++ SmoochAGhostTest/Assets/Booleans.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.IO;
+
+public class Booleans
+{
+    public static Booleans instance = new Booleans();
+    private Booleans()
+    {
+        Dictionary = new Dictionary<string, bool>();
+        Inventory = new Dictionary<string, bool>();
+
+        //BOOLS
+        string[] bools = File.ReadAllLines("Assets/Resources/Presets/Bools.txt");
+        foreach(string c in bools)
+        {
+            string[] parts = c.Trim().Split(':');
+            Dictionary.Add(parts[0].Trim(), (parts[1].Trim().ToUpper() == "TRUE"? true :false));
+        }
+
+        //ITEMS
+        string[] items = File.ReadAllLines("Assets/Resources/Presets/Items.txt");
+        foreach (string c in items)
+        {
+            string[] parts = c.Trim().Split(':');
+            Inventory.Add(parts[0].Trim(), (parts[1].Trim().ToUpper() == "TRUE" ? true : false));
+        }
+    }
+    public static Booleans GetInstance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+    public Dictionary<string, bool> Dictionary;
+    public Dictionary<string, bool> Inventory;
+
+}
---
+++ SmoochAGhostTest/Assets/Test.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class Test : MonoBehaviour {
+    [TextArea(10, 10)]
+    public string testString = "Ghost:1\n<HAPPY>%Hi I'm a test ghost\n<SAD>%I'm not in the game, I'm just hear to test... but here! Take this!\n[TAKE: APPLE]\n<HAPPY>%I just found it lying around, feel free to try it.\n#EAT%2:DONT EAT%3";
+    // Use this for initialization
+    void Start () {
+        Message testMessage = new Message(testString);
+        Debug.Log(testMessage.ToString());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
